8d0056a5e73f7ce99b654c643324a7b709a58099
diego-vini <analistamm@gmail.com>
2021-07-14 18:05:26 -0300

    Completando desafio final

--- 18-desafios_todos_conceitos_estudados/index.js
+++ 18-desafios_todos_conceitos_estudados/index.js
@@ -5,7 +5,7 @@ const user = {
 };
 
 function createTransaction(transaction){
-  user.transactions.push(transaction.value)
+  user.transactions.push(transaction)
   if(transaction.type == 'credit'){
       user.balance = user.balance + transaction.value
       
@@ -23,5 +23,29 @@ createTransaction({ type: "credit", value: 120 });
 createTransaction({ type: "debit", value: 80 });
 createTransaction({ type: "debit", value: 30 });
 
+function getHigherTransactionByType(type){
+    let pushValue
+    let startValue = 0;
+    for(let transaction of user.transactions ){
+    if(transaction.type == type && transaction.value > startValue){
+        startValue = transaction.value
+        pushValue = transaction
+    }
+    }
+    return pushValue;
+}
+
+function getAverageTransactionValue() {
+  let count = user.transactions.length
+  let sum = 0
+  for(let transaction of user.transactions){
+    sum = sum + transaction.value
+  }
+  
+  return sum / count
+}
 
 console.log(user)
+console.log('Maior transação de crédito: ', getHigherTransactionByType('credit'))
+console.log('Maior transação de débito: ', getHigherTransactionByType('debit'))
+console.log('A média das transações de crédito e débito é: ', getAverageTransactionValue())
